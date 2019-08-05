c657209aa17aa5ef130bd259262ea6832c606d7c
root <root@lord.localdomain>
2019-08-05 17:09:04 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /etc/profile
-SOURCE="http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone"
-#SOURCE="https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
+#SOURCE="http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone"
+SOURCE="https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
 #http://ipverse.net/ipblocks/data/countries/cn.zone
 wget -O ip_ranges.txt "$SOURCE"
 python3 ip-convert.py
